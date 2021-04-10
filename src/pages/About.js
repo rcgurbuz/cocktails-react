a30b7fceef051a5dc540827da80c6f24f6c25372
rcgurbuz <rifatcgurbuz@gmail.com>
2021-04-10 13:36:24 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
+        repellendus eaque dolore labore a? Omnis quae minima laborum
+        praesentium! Sit, explicabo voluptates. Nisi fugit quo nemo ipsam
+        nesciunt nam reiciendis harum odio similique, accusamus recusandae
+        sapiente qui, quia earum neque id voluptatum, quam nostrum assumenda
+        omnis. Quas culpa sapiente eos, pariatur aspernatur doloribus vel harum,
+        amet numquam adipisci reiciendis debitis voluptas asperiores corporis
+        quasi laborum alias quos perspiciatis architecto rem! Omnis accusamus
+        veniam fuga, provident nesciunt sed quidem beatae error cupiditate
+        nostrum praesentium reprehenderit doloribus atque laudantium qui natus
+        illum fugiat rerum, deserunt quod aperiam odit quae corporis! Beatae,
+        mollitia?
+      </p>
+    </section>
+  );
+};
+
+export default About;
